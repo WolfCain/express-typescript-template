e0ac56622afea8a600c96ec294339c82a4922c9d
WolfCain <wolfcain21@gmail.com>
2023-05-17 23:55:07 +0700

    Fix nodemon in container

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
+
+const ads = [
+    {title: 'Hello, world (again)!'}
+  ];
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+    res.send(ads);
+})
 
 app.listen(4000, () => {
     console.log('Server running on port 4000');
